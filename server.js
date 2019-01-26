2902d31202f2a2663e22682ce61fe5f8345cbcb7
Nat Grant <email.natgrant@gmail.com>
2019-01-26 16:53:15 +1300

    server-side setup

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const hbs = require('express-handlebars')
+const express = require("express");
+const hbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/users");
 
-const userRoutes = require('./routes/users')
-
-const server = express()
+const server = express();
 
 // Middleware
 
-server.engine('hbs', hbs({extname: 'hbs'}))
-server.set('view engine', 'hbs')
-server.use(express.urlencoded({extended: true}))
+server.engine("hbs", hbs({ extname: "hbs", defaultLayout: "main" }));
+server.set("view engine", "hbs");
+set.use(express.static("public"));
+server.use(bodyParser.json());
+server.use(express.urlencoded({ extended: true }));
 
 // Routes
 
-server.use('/', userRoutes)
+server.use("/", userRoutes);
 
-module.exports = server
+module.exports = server;
